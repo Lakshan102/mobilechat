a68901a58824fa531db90ec3f0378a3e8a84953f
lakshan102 <lakshan2725@gmail.com>
2024-07-20 10:10:56 +0530

    feat: Add navbar component and styling
    - Add navbar component to display the logo and app name
    - Style the navbar with a background color and font size
    - Make the navbar responsive for smaller screens
    
    Fixes #123

--- src/navBar.js
+++ src/navBar.js
@@ -3,7 +3,8 @@ import './navBar.css'
 
 function navBar() {
   return (
-    <div>
+    <div className='navBar'>
+      <img src='https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-1/425624086_931681941877187_6573070606561981615_n.jpg?stp=c0.0.200.200a_dst-jpg_p200x200&_nc_cat=109&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeHrgoDMVOIwSgBLlHrM0-WLt85IipFhgFu3zkiKkWGAW9OnCxDKd71GUFpyuGf5UnvnjhMW9QBBrjGx8ZGlW9Tz&_nc_ohc=sSHi2nt6Nd4Q7kNvgFaE2uy&_nc_ht=scontent-sin6-2.xx&oh=00_AYCPcS0wjvMrBIZANkVH_LbuNHR6GPlW0tAGGfM6_VwPNA&oe=66A10148' alt='logo' />
       <h1>Chat App</h1>
     </div>
   )
