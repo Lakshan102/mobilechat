e0220cb8779040f948a8ae6e6d57859571d41338
lakshan102 <lakshan2725@gmail.com>
2024-07-20 08:23:19 +0530

    use routes and create page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Routers,Routes,Route} from 'react-router-dom';
+import NavBar from './navBar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routers>
+        <NavBar/>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            {/* <Route path="/about" element={<AboutPage />} /> */}
+            
+          </Routes>
+      </Routers>
+      
     </div>
   );
 }
 
 export default App;
+
+/* import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './navBar'; // Corrected the import to follow React component naming conventions
+import Home from './Home';
+// import AboutPage from './AboutPage'; // Uncomment and import the AboutPage component if needed
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+         
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App; */
---
+++ src/navBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './navBar.css'
+
+function navBar() {
+  return (
+    <div>
+      <h1>Chat App</h1>
+    </div>
+  )
+}
+
+export default navBar;
